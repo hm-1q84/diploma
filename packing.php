1e4f1e5639e1f08829c3618900dc069aa9445b4a
hm-1q84 <39033315+hm-1q84@users.noreply.github.com>
2021-02-16 16:49:45 +0300

    form

--- packing.php
+++ packing.php
@@ -12,7 +12,7 @@
 <body>
 
     <?php include 'header.php' ?> <!-- dynamic header connection -->
-    
+
     <main>
         <div class="container">
             <div class="product">
@@ -226,8 +226,13 @@
             </div>
             <div class="row"> 
                 <div class="col-lg-6 offset-lg-3">
-                    <form action="" class="form_request">
-
+                    <form action="#" class="form form_request mx-auto">
+                        <h3 class="form__header">Оставить заявку</h3>
+                        <input class="form__input" type="text" placeholder="Имя">
+                        <input class="form__input" type="text" placeholder="Телефон">
+                        <input class="form__input" type="email" placeholder="Email">
+                        <textarea class="form__input form__textarea" cols="30" rows="5" placeholder="Комментарий"></textarea>
+                        <input class="form__btn" type="submit" value="Отправить">
                     </form>
                 </div>
             </div>
