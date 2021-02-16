3981061c89b4802bfc19c21df12b421d1966be7c
hm-1q84 <39033315+hm-1q84@users.noreply.github.com>
2021-02-16 13:57:09 +0300

    packing

--- index.php
+++ index.php
@@ -68,14 +68,6 @@
             </div>
         </div>
     </main>
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <p class="footer__info">ООО "СМС"<br>2020</p>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <?php include("footer.php"); ?>    <!-- dynamic footer connection -->
 </body>
 </html>
